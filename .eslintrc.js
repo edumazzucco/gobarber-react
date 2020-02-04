20bae5d3424102f9826d6dbdee8ff3fb48d83066
edumazzucco <edumazzucco@hotmail.com>
2020-02-04 20:05:03 -0300

    feat: private routes, global styles and _layouts set

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     rules: {
         "prettier/prettier": "error",
         "react/jsx-filename-extension": ["error", { extensions: [".js", "jsx"] }],
+        "react/jsx-props-no-spreading": "off",
         "import/prefer-default-export": "off",
         "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
         "react/jsx-one-expression-per-line": "off",
@@ -30,6 +31,6 @@ module.exports = {
         camelcase: "off",
         "no-console": ["error", { allow: ["tron"] }],
         "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
+        "react-hooks/exhaustive-deps": "warn",
     }
 };
