1de7ef88f5a6cd5eb9e4ced29cde33c1e12dbf9f
edumazzucco <edumazzucco@hotmail.com>
2020-02-05 18:17:30 -0300

    feat: signup, header style

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -22,6 +22,8 @@ export function* signIn({ payload }) {
             return;
         }
 
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
         yield put(signInSuccess(token, user));
 
         history.push('/dashboard');
@@ -33,4 +35,38 @@ export function* signIn({ payload }) {
     }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+    try {
+        const { name, email, password } = payload;
+
+        yield call(api.post, 'users', {
+            name,
+            email,
+            password,
+            provider: true,
+        });
+
+        history.push('/');
+        toast.success('Account created, now you can login!');
+    } catch (err) {
+        toast.error('Registration failed, please check the required fields.');
+
+        yield put(signFailure());
+    }
+}
+
+export function setToken({ payload }) {
+    if (!payload) return;
+
+    const { token } = payload.auth;
+
+    if (token) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
+
+export default all([
+    takeLatest('persist/REHYDRATE', setToken),
+    takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+    takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
