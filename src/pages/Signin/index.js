453c8b9601c26042c134c6fb3122c37afc067c41
edumazzucco <edumazzucco@hotmail.com>
2020-02-05 11:50:23 -0300

    feat: babel root import, login and register style, unform installed, front-end form validation

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insert a valid e-mail address')
+        .required('An email address is required'),
+    password: Yup.string().required('An password is required'),
+});
 
 export default function Signin() {
-    return <h1>Login</h1>;
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+    return (
+        <>
+            <img src={logo} alt="GoBarber" />
+
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Your email address"
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Your password"
+                />
+
+                <button type="submit">Login</button>
+                <Link to="/register">Create a free account</Link>
+            </Form>
+        </>
+    );
 }
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-export default function Signup() {
-    return <div />;
+const schema = Yup.object().shape({
+    name: Yup.string().required('Your full name is required'),
+    email: Yup.string()
+        .email('Insert a valid e-mail address')
+        .required('An email address is required'),
+    password: Yup.string()
+        .min(6, 'Password must contain at least 6 characters')
+        .required('An password is required'),
+});
+
+export default function SignUp() {
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+    return (
+        <>
+            <img src={logo} alt="GoBarber" />
+
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Your full name" />
+                <Input name="email" type="email" placeholder="Your email" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Your password"
+                />
+
+                <button type="submit">Create account</button>
+                <Link to="/">I already have an account</Link>
+            </Form>
+        </>
+    );
 }
